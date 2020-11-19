20cc92e826cad8b7e9204107e9c45b367f3e98d4
samuelrrs <samuellrrs@gmail.com>
2020-11-19 18:05:26 -0300

    Add: Desafio 5

---
+++ Desafio 5 JS..js
@@ -0,0 +1,19 @@
+var usuarios = [
+    {
+    nome: "Diego",
+    habilidades: ["Javascript", "ReactJS", "Redux"]
+    },
+    {
+    nome: "Gabriel",
+    habilidades: ["VueJS", "Ruby on Rails", "Elixir"]
+    }
+   ];
+
+   
+   console.log(
+    `O ${usuarios[0].nome}  as habilidades: ${usuarios[0].habilidades} `
+  );
+   console.log(
+    `O ${usuarios[1].nome}  as habilidades: ${usuarios[1].habilidades} `
+  );
+  
