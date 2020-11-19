23c26705eaac70004324c0c2420c2dcb1256ed3e
samuelrrs <samuellrrs@gmail.com>
2020-11-19 16:44:23 -0300

    first commit

---
+++ Desafio 1 JS.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Exercicio 1</title>
+  </head>
+
+  <body>
+    <script>
+      var endereco = {
+        rua: "Rua dos pinheiros",
+        numero: 1293,
+        bairro: "Centro",
+        cidade: "São Paulo",
+        uf: "SP",
+      };
+      console.log(
+        `O usuáio mora em ${endereco.cidade} / ${endereco.uf} no bairro ${endereco.bairro}, na rua ${endereco.rua} com o número ${endereco.numero}`
+      );
+    </script>
+  </body>
+</html>
---
+++ Desafio 2 JS.js
@@ -0,0 +1,9 @@
+const apenasPares = (x, y) => {
+  for (var i = x; i <= y; i++) {
+    if (i % 2 === 0) {
+      console.log(i);
+    }
+  }
+};
+
+apenasPares(1, 20)
