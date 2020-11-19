17f601ec3bdcd1b81d3160358f8d0e81cc20d04f
samuelrrs <samuellrrs@gmail.com>
2020-11-19 18:14:57 -0300

    Add: Desafio 5

--- Desafio 5 JS..js
+++ Desafio 5 JS..js
@@ -1,19 +1,18 @@
 var usuarios = [
     {
-    nome: "Diego",
-    habilidades: ["Javascript", "ReactJS", "Redux"]
+      nome: "Diego",
+      habilidades: ["Javascript", "ReactJS", "Redux"]
     },
-    {
-    nome: "Gabriel",
-    habilidades: ["VueJS", "Ruby on Rails", "Elixir"]
-    }
-   ];
-
-   
-   console.log(
-    `O ${usuarios[0].nome}  as habilidades: ${usuarios[0].habilidades} `
-  );
-   console.log(
-    `O ${usuarios[1].nome}  as habilidades: ${usuarios[1].habilidades} `
-  );
+    { nome: "Gabriel", habilidades: ["VueJS", "Ruby on Rails", "Elixir"] }
+  ];
   
+  const mostrarMsg = (usuarios) => {
+    
+    for( var usuario of usuarios) {
+        const {nome, habilidades} = usuario
+        console.log(`O ${nome} possui as habilidades: ${habilidades.join(", ")}`);
+    }
+
+  };
+
+  mostrarMsg(usuarios)
